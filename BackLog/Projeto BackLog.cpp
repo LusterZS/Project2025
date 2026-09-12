13a71bfd011e0524acd2c95140d61b8ded767476
LusterZS <thelusterk@gmail.com>
2026-09-11 21:33:27 -0300

    Alteração pasta

---
+++ BackLog/Projeto BackLog.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(){
+
+char nome[10];
+
+printf("Projeto BANCO DE DADOS V2");
+  sleep(2);
+  printf("\n");
+ printf("Digite Seu User: ");
+ scanf("%c", nome);
+
+}
